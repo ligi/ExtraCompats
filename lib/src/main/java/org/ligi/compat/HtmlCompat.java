76a0c560ffc6e7015cf49fee4db7effda877e79b
ligi <ligi@ligi.de>
2016-10-26 15:48:04 +0200

    Add another method signature for fromHtml with imagegetter

--- lib/src/main/java/org/ligi/compat/HtmlCompat.java
+++ lib/src/main/java/org/ligi/compat/HtmlCompat.java
@@ -9,7 +9,17 @@ public class HtmlCompat {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             return Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY);
         } else {
+            //noinspection deprecation
             return Html.fromHtml(source);
+        }
+    }
+
+    public static Spanned fromHtml(String source, Html.ImageGetter imageGetter, Html.TagHandler tagHandler) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            return Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY, imageGetter, tagHandler);
+        } else {
+            //noinspection deprecation
+            return Html.fromHtml(source, imageGetter, tagHandler);
         }
     }
 
